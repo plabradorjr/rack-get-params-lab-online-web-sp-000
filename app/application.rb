68709a197ec60503f723afaa7f063f47ab742a0f
Pancho Labrador <plabradorjr@gmail.com>
2020-01-10 15:56:40 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -22,7 +22,15 @@ class Application
           resp.write "#{item}\n"
         end
       end
-      
+
+    elsif req.path.math(/add/)
+      item_to_add = req.params["item"]
+      if @@items.include? item_to_add
+        @@cart << item_to_add
+        resp.write "added #{item_to_add}"
+      else
+        resp.write "We don't have that item!"
+      end 
     else
       resp.write "Path Not Found"
     end
